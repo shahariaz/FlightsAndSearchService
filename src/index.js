db43d1e30750d04bc921c5618dd82466c22cc5fa
shahariaz <shahariaz.info@gmail.com>
2023-12-26 15:37:07 +0600

    setup bodyparser and clean code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const { PORT } = require("./config/serverConfig.js");
 const setupAndStartServer = async () => {
   const app = express();
-  app.get("/", (req, res) => {
-    res.send("Hello World!");
-  });
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
   app.listen(PORT);
   console.log(`Server listening on ${PORT}`);
 };
