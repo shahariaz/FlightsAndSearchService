a61f5442cca9f12d961a30357acfd73fca43527d
shahariaz <shahariaz.info@gmail.com>
2023-12-26 15:24:41 +0600

    setup basic env file

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const { PORT } = require("./config/serverConfig.js");
+const setupAndStartServer = async () => {
+  const app = express();
+  app.get("/", (req, res) => {
+    res.send("Hello World!");
+  });
+  app.listen(PORT);
+  console.log(`Server listening on ${PORT}`);
+};
+setupAndStartServer();
